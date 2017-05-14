23959e9e1d912c36937bdd28e7ef727c261c8ce0
fab <fabvest7@gmail.com>
2017-05-14 19:22:32 +0300

    Now phrase gets from forismatic

--- src/main/java/ComeOnDudeBot.java
+++ src/main/java/ComeOnDudeBot.java
@@ -6,13 +6,14 @@ import org.telegram.telegrambots.exceptions.TelegramApiException;
 
 import java.util.Date;
 import java.util.Random;
+import java.util.logging.Logger;
 
-/**
- * Created by fab on 12.05.2017.
- */
 public class ComeOnDudeBot extends TelegramLongPollingBot {
+    private static Logger log = Logger.getLogger(ComeOnDudeBot.class.getName());
     public void onUpdateReceived(Update update) {
-        Random r = new Random();
+        log.info("Update");
+        //Random r = new Random();
+        log.info(" users msg: " + update.getMessage().getText());
         String msg = update.getMessage().getText();
 
         // We check if the update has a message and the message has text
@@ -24,14 +25,14 @@ public class ComeOnDudeBot extends TelegramLongPollingBot {
 //
             if(msg.equals("/getphrase")){
                 Forismatic.Quote q = new Forismatic().getQuote();
-                if(!q.getQuoteAuthor().equals(null) && !q.getQuoteText().equals(null)) {
+                if(!q.getQuoteAuthor().equals(null) && !q.getQuoteText().equals("")) {
                     message.setText(q.getQuoteText() + "\n" + q.getQuoteAuthor() + "\n\n"
                             + "via forismatic.com");
-                }else if(q.getQuoteAuthor().equals(null)){
+                }else if(q.getQuoteAuthor().equals("")){
                     message.setText(q.getQuoteText() + "\n"
                             + "via forismatic.com");
                 }
-
+                log.info(" phrase: " + q.getQuoteText() + " " + q.getQuoteAuthor());
             }else if(msg.equals("/help")){
                 message.setText("/getphrase - прислать какую-либо фразу" + "\n"
                                     + "/gettime - подстказать время" + "\n"
@@ -58,16 +59,16 @@ public class ComeOnDudeBot extends TelegramLongPollingBot {
         return "357160314:AAE3QMcCHHW3VrvKni92Xpr4VOUNzWsUknI";
     }
 
-    String[] s = {
-            "Каждый бот для чего-то создан, но создан человеком",
-            "Только люди могут быть творцами ботов",
-            "Сначала появился человек, а потом - бот",
-            "Кто старое помянет - тому глаз вон",
-            "Подлецу все к лицу",
-            "Наполни день смыслом. Ну, или не наполняй",
-            "Ты можешь сделать все, что захочешь. Ну, если у тебя есть время, " +
-                    "здоровье, деньги, власть, ум и еще пару тройку незаменимых качеств",
-            "Семь раз отмерь, один раз все брось.",
-            "Без труда не вытащишь и рыбку из пруда, но ее без труда можно украсть"
-    };
+//    String[] s = {
+//            "Каждый бот для чего-то создан, но создан человеком",
+//            "Только люди могут быть творцами ботов",
+//            "Сначала появился человек, а потом - бот",
+//            "Кто старое помянет - тому глаз вон",
+//            "Подлецу все к лицу",
+//            "Наполни день смыслом. Ну, или не наполняй",
+//            "Ты можешь сделать все, что захочешь. Ну, если у тебя есть время, " +
+//                    "здоровье, деньги, власть, ум и еще пару тройку незаменимых качеств",
+//            "Семь раз отмерь, один раз все брось.",
+//            "Без труда не вытащишь и рыбку из пруда, но ее без труда можно украсть"
+//    };
 }
